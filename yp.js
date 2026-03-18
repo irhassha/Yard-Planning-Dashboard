b65973a058470c208a62212ffe950b53c54ff8e6
irhassha <irhassha@gmail.com>
2026-03-18 11:07:50 +0700

    Update yp.js

--- yp.js
+++ yp.js
@@ -127,7 +127,7 @@ function updateCapacity(block, newSlots, newTier) {
 }
 
 
-    const PORTAL_URL = "https://irhassha.github.io/planning-portal/";
+    const PORTAL_URL = "https://irhassha.github.io/Planning-Portal/";
     const SUPABASE_URL = "https://ipkgtglrhnocatrqfsda.supabase.co";
     const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwa2d0Z2xyaG5vY2F0cnFmc2RhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3MjIxNzMsImV4cCI6MjA4OTI5ODE3M30.tHQ6IzJEMoQicDPfhgGIYPCk-6fmgJlHGv1VvsAHrx0";
     
